df217d889a5f1d27014627427fa7290e3ed94438
jamie.cornick <jamie.cornick@keyin.com>
2022-07-18 09:52:54 -0230

    Added first and last name search.

--- src/main/java/com/keyin/rest/domain/MemberRepository.java
+++ src/main/java/com/keyin/rest/domain/MemberRepository.java
@@ -2,10 +2,13 @@ package com.keyin.rest.domain;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "member", path = "member")
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
-
+    public List<Member> findByLastName(@Param("last_name")String lastName);
+    public List<Member> findByFirstName(@Param("first_name")String firstName);
 }
